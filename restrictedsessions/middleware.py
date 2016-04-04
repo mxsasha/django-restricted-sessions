19a5ff950905615d69da1cf5b67d6f6eb8f38e01
webrunners <projekte@webrunners.de>
2016-04-04 13:15:16 +0200

    Extracted all static variables to init-time
    Reduce number of run-time operations in favor of init-time code. Use settings only for things could change in run-time.

--- restrictedsessions/middleware.py
+++ restrictedsessions/middleware.py
@@ -9,6 +9,10 @@ from django.contrib.auth import logout
 SESSION_IP_KEY = '_restrictedsessions_ip'
 SESSION_UA_KEY = '_restrictedsessions_ua'
 AUTHED_ONLY = getattr(settings, 'RESTRICTEDSESSIONS_AUTHED_ONLY', False)
+IP_HEADER = getattr(settings, 'RESTRICTEDSESSIONS_REMOTE_ADDR_KEY', 'REMOTE_ADDR')
+UA_HEADER = 'HTTP_USER_AGENT'
+IPV4_LENGTH = getattr(settings, 'RESTRICTEDSESSIONS_IPV4_LENGTH', 32)
+IPV6_LENGTH = getattr(settings, 'RESTRICTEDSESSIONS_IPV6_LENGTH', 64)
 
 logger = logging.getLogger('restrictedsessions')
 
@@ -23,8 +27,7 @@ class RestrictedSessionsMiddleware(object):
         if AUTHED_ONLY and (user is None or not user.is_authenticated()):
             return
 
-        remote_addr_key = getattr(settings, 'RESTRICTEDSESSIONS_REMOTE_ADDR_KEY', 'REMOTE_ADDR')
-        remote_addr = request.META.get(remote_addr_key)
+        remote_addr = request.META.get(IP_HEADER)
         if not remote_addr:
             return
 
@@ -34,8 +37,8 @@ class RestrictedSessionsMiddleware(object):
             logout(request)
 
         request.session[SESSION_IP_KEY] = remote_addr
-        if request.META.get('HTTP_USER_AGENT'):
-            request.session[SESSION_UA_KEY] = request.META['HTTP_USER_AGENT']
+        if request.META.get(UA_HEADER):
+            request.session[SESSION_UA_KEY] = request.META[UA_HEADER]
 
     def validate_ip(self, request, remote_ip):
         if not getattr(settings, 'RESTRICTEDSESSIONS_RESTRICT_IP', True) or not SESSION_IP_KEY in request.session:
@@ -46,10 +49,10 @@ class RestrictedSessionsMiddleware(object):
         try:
             session_network = session_network.ipv4()
             remote_ip = remote_ip.ipv4()
-            session_network.prefixlen = getattr(settings, 'RESTRICTEDSESSIONS_IPV4_LENGTH', 32)
+            session_network.prefixlen = IPV4_LENGTH
         except AddrConversionError:
             try:
-                session_network.prefixlen = getattr(settings, 'RESTRICTEDSESSIONS_IPV6_LENGTH', 64)
+                session_network.prefixlen = IPV6_LENGTH
             except AddrFormatError:
                 # session_network must be IPv4, but remote_ip is IPv6
                 return False
@@ -58,4 +61,4 @@ class RestrictedSessionsMiddleware(object):
     def validate_ua(self, request):
         if not getattr(settings, 'RESTRICTEDSESSIONS_RESTRICT_UA', True) or not SESSION_UA_KEY in request.session:
             return True
-        return request.session[SESSION_UA_KEY] == request.META.get('HTTP_USER_AGENT')
+        return request.session[SESSION_UA_KEY] == request.META.get(UA_HEADER)
